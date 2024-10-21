9be3786432f68165245f6064c5dc2a9172b471fc
Russell <russelldcosta7@gmail.com>
2024-10-21 18:55:47 +0530

    noticeboard-static

--- bandobast/settings.py
+++ bandobast/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'bandobast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',                           #FOR MYSQL
+#         'NAME': 'bandobast',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'Sylvan123*'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',                           #FOR MYSQL
-        'NAME': 'bandobast',
+        'NAME': 'traffic_management',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'Sylvan123*'
+        'PASSWORD': 'password'
     }
 }
 
